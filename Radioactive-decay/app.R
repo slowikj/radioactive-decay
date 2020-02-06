be98985851229f6ba7b1130b78c197b9ca37a991
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-02-06 19:45:31 +0100

    Add the base shiny aapp

---
+++ Radioactive-decay/app.R
@@ -0,0 +1,53 @@
+library(shiny)
+library(plotly)
+library(shinycssloaders)
+
+source("plot.R")
+
+isotopes <- read.csv(file="./isotopes.csv")
+rownames(isotopes) <- isotopes$name
+print(isotopes)
+
+ui <- fluidPage(
+    titlePanel("Radioactive decay Monte Carlo simulation"),
+
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(inputId = "isotope",
+                        label = "Isotope",
+                        choices = isotopes$name),
+            numericInput(inputId = "objects_number",
+                        label = "Objects number",
+                        min = 1,
+                        value = 1000,
+                        max=1000000000),
+            numericInput(inputId = "steps_number",
+                        label = "Number of steps",
+                        min = 1,
+                        value = 5,
+                        max=500),
+            numericInput(inputId = "step_length",
+                        label = "Step length [years]",
+                        min = 1,
+                        value = 5,
+                        max=1000)
+        ),
+
+        mainPanel(
+            withSpinner(plotlyOutput("plot"))
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$plot <- renderPlotly({
+        ggplotly(get_plot(start_objects_number=input$objects_number,
+                          half_life=isotopes[input$isotope,]$half_life,
+                          time_step_number=input$steps_number,
+                          time_step_length=input$step_length))
+    })
+}
+
+shinyApp(ui = ui, server = server)
---
+++ Radioactive-decay/plot.R
@@ -0,0 +1,34 @@
+library(ggplot2)
+library(magrittr)
+library(reshape2)
+source("formulas.R")
+
+get_plot <- function(start_objects_number, half_life, time_step_number, time_step_length) {
+  max_time <- time_step_number * time_step_length
+  time_steps <- seq(0, max_time, time_step_length)
+
+  theory_objects_number <- get_objects_number_theoretically(start_objects_number, half_life)(time_steps)
+  experiment_objects_number <- get_experimental_objects_number(start_objects_number,
+                                                               half_life,
+                                                               time_step_number,
+                                                               time_step_length)
+  
+  df <- data.frame(
+    time = time_steps,
+    theory_objects_number = theory_objects_number,
+    experiment_objects_number = experiment_objects_number
+  ) %>% 
+    melt(id.vars=c("time"),
+         measure.vars=c("theory_objects_number", "experiment_objects_number"))
+
+  df %>%
+    ggplot(aes(x = time, y = value, color=variable)) +
+    geom_line() +
+    geom_point() +
+    scale_color_hue(labels=c("theory", "experiment")) +
+    labs(color="type") +
+    xlab("time [years]") +
+    ylab("available objects") +
+    scale_x_continuous(breaks = seq(0, max_time, time_step_length)) +
+    theme_classic()
+}
