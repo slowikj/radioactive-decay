19ba386a043383c7a876501e964a20a33063ca23
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-02-06 19:55:21 +0100

    Clean up + add more isotopes

--- Radioactive-decay/formulas.R
+++ Radioactive-decay/formulas.R
@@ -15,11 +15,12 @@ get_experimental_objects_number <- function(start_objects_number, half_life, tim
   current_objects_number <- start_objects_number
   current_time <- 0
   res <- rep(NA, time_step_number)
-  for (i in 1:(time_step_number + 1)) {
+  time_elapsed_seq <- seq(0, time_step_number * time_step_length, time_step_length)
+  for (i in 1:length(time_elapsed_seq)) {
+    current_time <- time_elapsed_seq[i]
     life_probability <- life_probability_fun(current_time)
     current_objects_number <- sum(runif(current_objects_number, 0, 1) < life_probability)
     res[i] <- current_objects_number
-    current_time <- current_time + time_step_length
   }
   res
 }
